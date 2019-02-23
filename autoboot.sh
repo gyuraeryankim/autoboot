3606be25148a539b03c44f1d50789567365e025a
gyuraeryankim <40163522+gyuraeryankim@users.noreply.github.com>
2019-02-23 10:51:36 +0900

    Update autoboot.sh

--- autoboot.sh
+++ autoboot.sh
@@ -13,7 +13,7 @@ killall nodeos
 ./cleos.sh wallet unlock < ./walletpassword.txt &> /dev/null
 
 #run eosio node. see datadir for logs
-nodeos -e -p eosio --plugin eosio::producer_plugin --max-transaction-time=300 --plugin eosio::chain_api_plugin --plugin eosio::net_api_plugin --signature-provider=EOS5kiTBoD38FxXTmcPzJS3EjtcusKE2WkEfjGEPFTnnth21hQMHa=KEY:5KX9iLDRus4jevcBRPArmPRrkQFD9MBtTEdjAxnYMR5a9wcs46W --genesis-json /home/eos/ibctprivate/genesis.json --config-dir /home/eos/ibctprivate --delete-all-blocks > /home/eos/ibctprivate/stdout.txt 2> /home/eos/ibctprivate/stderr.txt & echo $! > /home/eos/ibctprivate/nodoes.pid
+/home/eos/eos/build/programs/nodeos/nodeos -e -p eosio --plugin eosio::producer_plugin --max-transaction-time=300 --plugin eosio::chain_api_plugin --plugin eosio::net_api_plugin --signature-provider=EOS5kiTBoD38FxXTmcPzJS3EjtcusKE2WkEfjGEPFTnnth21hQMHa=KEY:5KX9iLDRus4jevcBRPArmPRrkQFD9MBtTEdjAxnYMR5a9wcs46W --genesis-json /home/eos/ibctprivate/genesis.json --config-dir /home/eos/ibctprivate --delete-all-blocks > /home/eos/ibctprivate/stdout.txt 2> /home/eos/ibctprivate/stderr.txt & echo $! > /home/eos/ibctprivate/nodoes.pid
 
 #give time for the node to be initialized.
 sleep 2s
@@ -39,7 +39,7 @@ sleep 2s
 sleep 2s
 
 ./cleos.sh system regproducer ryangenesis1 EOS57wGFHsp85BV2o7WBfTLibUX8ZsbwFK5THmAQ6seKbgT3fsG3X "ibctmini.io" -p ryangenesis1
-./cleos.sh system delegatebw ryangenesis1 ryangenesis1  "75000000.0000 CR" "75000000.0000 CR" 
+./cleos.sh system delegatebw ryangenesis1 ryangenesis1  "75000000.0000 EOS" "75000000.0000 EOS" 
 ./cleos.sh system voteproducer prods ryangenesis1 ryangenesis1
 # at this point you will see ryangensis1 as a head block producer
 
