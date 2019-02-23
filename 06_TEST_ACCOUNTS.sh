629718c851de92f1ebf003a7735e90ae3d2ee1d0
gyuraeryankim <40163522+gyuraeryankim@users.noreply.github.com>
2019-02-23 10:53:02 +0900

    Update 06_TEST_ACCOUNTS.sh

--- 06_TEST_ACCOUNTS.sh
+++ 06_TEST_ACCOUNTS.sh
@@ -11,6 +11,6 @@
 INIT_ACCOUNT="$( jq -r '.INIT_ACCOUNT' "00_CONFIG.conf" )"
 INIT_PUB_KEY="$( jq -r '.INIT_PUB_KEY' "00_CONFIG.conf" )"
 
-./cleos.sh system newaccount --stake-net "150000000.0000 CR" --stake-cpu "150000000.0000 CR" --buy-ram "10000.0000 CR" eosio $INIT_ACCOUNT $INIT_PUB_KEY $INIT_PUB_KEY -p eosio
+./cleos.sh system newaccount --stake-net "150000000.0000 EOS" --stake-cpu "150000000.0000 EOS" --buy-ram "10000.0000 EOS" eosio $INIT_ACCOUNT $INIT_PUB_KEY $INIT_PUB_KEY -p eosio
 
 
